a9bc802d23e4ae9935e299f5a957e220e40f4eb2
Iain Earl <synapse791@gmail.com>
2016-11-10 15:45:58 +0000

    Adding extra helper methods

--- app/Services/BaseService.php
+++ app/Services/BaseService.php
@@ -3,6 +3,7 @@
 namespace Keikaku\Services;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Keikaku\Contracts\Services\ServiceErrors;
 
@@ -75,111 +76,142 @@ class BaseService implements ServiceErrors
     }
 
     /**
-     * @param string $message
+     * Shortcut to set 400 Bad Request error
+     *
+     * @param string|array $message
      * @return bool
      */
-    protected function setBadRequest(string $message): bool
+    public function setBadRequestError($message)
     {
         $this
             ->setError('bad_request')
             ->setErrorCode(400)
             ->setErrorDescription($message);
-
         return false;
     }
-
     /**
-     * @param BaseService $service
+     * Shortcut to set 404 Not Found error
+     *
+     * @param string|array $message
      * @return bool
      */
-    public function setErrorFromService(BaseService $service)
+    public function setNotFoundError($message)
     {
-        $this->setErrorCode($service->getErrorCode())
-            ->setError($service->getError())
-            ->setErrorDescription($service->getErrorDescription());
+        $this
+            ->setError('not_found')
+            ->setErrorCode(404)
+            ->setErrorDescription($message);
         return false;
     }
-
     /**
+     * Shortcut to set 409 Conflict error
+     *
+     * @param string|array $message
+     * @return bool
+     */
+    public function setConflictError($message)
+    {
+        $this
+            ->setError('conflict')
+            ->setErrorCode(409)
+            ->setErrorDescription($message);
+        return false;
+    }
+    /**
+     * Shortcut to set 500 Internal Server error
+     *
+     * @param string|array $message
+     * @return bool
+     */
+    public function setInternalServerError($message)
+    {
+        $this
+            ->setError('internal_server_error')
+            ->setErrorCode(500)
+            ->setErrorDescription($message);
+        return false;
+    }
+    /**
+     * Tries to save an entity
+     *
+     * @param Model $entity
+     * @param null $conflictMessage
+     * @return bool
+     */
+    public function saveEntity($entity, $conflictMessage = null)
+    {
+        try {
+            $entity->save();
+            return true;
+        } catch (\PDOException $e) {
+            if (str_contains($e->getMessage(), 'Duplicate entry'))
+                return is_null($conflictMessage)
+                    ? $this->setConflictError(get_class($entity) . ' already exists')
+                    : $this->setConflictError($conflictMessage);
+            return $this->setInternalServerError($e->getMessage());
+        }
+    }
+    /**
+     * Tries to associate an entity with a relationship
+     *
+     * @param BelongsTo $relationship
      * @param Model $entity
      * @return bool
      */
-    protected function saveEntity(Model $entity): bool
+    public function associateEntities(BelongsTo $relationship, Model $entity)
     {
         try {
-            return $entity->save();
+            $relationship->associate($entity);
+            return true;
         } catch (\PDOException $e) {
-            if (str_contains($e->getMessage(), 'Duplicate entry')) {
-                $this
-                    ->setError('conflict')
-                    ->setErrorCode(409)
-                    ->setErrorDescription($e->getMessage());
-            } else {
-                $this
-                    ->setError('database_error')
-                    ->setErrorCode(500)
-                    ->setErrorDescription($e->getMessage());
-            }
-
-            return false;
+            return $this->setInternalServerError($e->getMessage());
         }
     }
-
     /**
+     * Tries to dissociate a relationship
+     *
+     * @param BelongsTo $relationship
+     * @return bool
+     */
+    public function dissociateEntities(BelongsTo $relationship)
+    {
+        try {
+            $relationship->dissociate();
+            return true;
+        } catch (\PDOException $e) {
+            return $this->setInternalServerError($e->getMessage());
+        }
+    }
+    /**
+     * Tries to attach an entity to a relationship
+     *
+     * @param BelongsToMany $relationship
      * @param Model $entity
      * @return bool
      */
-    protected function deleteEntity(Model $entity): bool
+    public function attachEntities(BelongsToMany $relationship, Model $entity)
     {
         try {
-            return $entity->delete();
+            $relationship->attach($entity);
+            return true;
         } catch (\PDOException $e) {
-            $this
-                ->setError('database_error')
-                ->setErrorCode(500)
-                ->setErrorDescription($e->getMessage());
-
-            return false;
+            return $this->setInternalServerError($e->getMessage());
         }
     }
-
     /**
+     * Tries to detach an entity to a relationship
+     *
      * @param BelongsToMany $relationship
-     * @param Model $toBeAttached
+     * @param Model $entity
      * @return bool
      */
-    public function attachEntities(BelongsToMany $relationship, Model $toBeAttached)
+    public function detachEntities(BelongsToMany $relationship, Model $entity)
     {
         try {
-            $relationship->attach($toBeAttached);
+            $relationship->detach($entity);
             return true;
-        } catch (\Exception $e) {
-            $this
-                ->setError('database_error')
-                ->setErrorCode(500)
-                ->setErrorDescription($e->getMessage());
-
-            return false;
-        }
-    }
-
-    /**
-     * @param BelongsToMany $relationship
-     * @param Model $toBeDetached
-     * @return bool
-     */
-    public function detachEntities(BelongsToMany $relationship, Model $toBeDetached)
-    {
-        try {
-            $relationship->detach($toBeDetached);
-            return true;
-        } catch (\Exception $e) {
-            $this
-                ->setError('database_error')
-                ->setErrorCode(500)
-                ->setErrorDescription($e->getMessage());
-
-            return false;
+        } catch (\PDOException $e) {
+            return $this->setInternalServerError($e->getMessage());
         }
     }
 }
